aa829d5ea8e1b674d77c45c3bb3ae77cc096dea8
Levi Wenger <myeyesareonmizar@gmail.com>
2021-03-23 20:15:45 -0400

    Added filter.

--- discordBot.js
+++ discordBot.js
@@ -2,6 +2,12 @@ const Discord = require("discord.js");
 const channelId = "770264950915596320";
 const ownerId = "770262647081402418";
 const cqptainId = "375452395733254144";
+const filter = [
+    "nig",
+    "neg",
+    "n6",
+    "n9"
+]
 require('dotenv').config();
 
 var client;
@@ -23,6 +29,9 @@ module.exports.run = async () => {
         if(message.channel.id != channelId) return;
         if(message.author.bot) return;
 
+        // chat filter
+        if(filterCheck(message.content)) return;
+
         // allows owners to execute commands from discord
         if((message.guild.member(message.author).roles.get(ownerId)/* || message.author.id == cqptainId*/) && message.content.startsWith("/")){
             client.write("chat", {message:`${message.content}`})
@@ -36,6 +45,14 @@ module.exports.run = async () => {
 
     // logs in the bot 
     bot.login(process.env.TOKEN);
+}
+
+function filterCheck(message){
+    let outcome = false;
+    filter.forEach(val => {
+        if(message.includes(val)) outcome = true;
+    })
+    return outcome;
 }
 
 // function to set client
