85892c2466051ef7cb72c91e50e71140b6fcfbbc
Frostingly <doltymax112@gmail.com>
2021-05-11 20:19:57 +0300

    fixed

--- commands/link.js
+++ commands/link.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 
+/*
 module.exports = {
     name: 'link',
     execute(message, args) {
@@ -7,10 +8,12 @@ module.exports = {
             .setColor("GOLD");
 
         // generates number and adds it to the currentlyLinking
-        let currentlyLinking = require('./main').currentlyLinking;
+        let currentlyLinking = require('../index').currentlyLinking;
         let number = Math.floor((Math.random() * 9999) + 1000);
         currentlyLinking[number] = message.author.id;
 
         message.channel.send(embed.setColor("GREEN").setDescription(`Your code is ${number}. Message InfluxBot on minecraft 'link ${number}' to link your account.`));
     }
 }
+
+ */
