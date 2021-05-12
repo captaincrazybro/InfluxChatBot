0401a5fe97749b710f06205ea71371094a6da6a3
Frostingly <doltymax112@gmail.com>
2021-05-12 21:10:41 +0300

    put the cooldown in a hierchy in the purge cmd

--- commands/purge.js
+++ commands/purge.js
@@ -8,9 +8,9 @@ module.exports = {
     async execute(message, args) {
         let embed = new Discord.MessageEmbed()
             .setColor("GOLD");
-        if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(embed.setDescription("You do not have permission to use this command.")).then(msg => msg.delete({timeout: 3000}))
         if (index.currentlyCooldowned[message.author.id] === message.author.id) return message.reply(embed.setDescription("Please wait a few seconds before executing the command again!")).then(msg => msg.delete( {timeout: 3000} ).then(message.delete( {timeout: 2000} )))
         functions.setCooldown(5, message.author.id, message);
+        if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(embed.setDescription("You do not have permission to use this command.")).then(msg => msg.delete({timeout: 3000}))
         let amount = args[0];
         if (amount === null || amount === undefined || isNaN(amount)) return message.reply(embed.setDescription('Please input an amount (e.g. `.clear 100`).')).then(msg => msg.delete( {timeout: 3000} ));
         amount = parseInt(amount);
