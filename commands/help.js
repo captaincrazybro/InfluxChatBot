07b4e23594f50176071b07f9cf1810def680a411
Levi Wenger <myeyesareonmizar@gmail.com>
2021-05-12 14:21:34 -0400

    Edited help command.

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const prefix = "."
-const index = require('../index');
+const index = ".";
 const functions = require('../Functions');
 
 module.exports = {
@@ -14,7 +14,9 @@ module.exports = {
             .addField(`${prefix}leaderboard`, "Shows the today's daily gexp leaderboard.")
             .addField(`${prefix}link <code>`, "Links your minecraft account.")
             .addField(`${prefix}listblacklists`, "Lists the currently blacklisted users.")
-            .addField(`${prefix}player <user>`, "Gets a user's minecraft account information.");
+            .addField(`${prefix}player <user>`, "Gets a user's minecraft account information.")
+            .addField(`${prefix}purge <amount>`, "Purges a certain number of messages in a channel.")
+            .addField(`${prefix}removeblacklist <user>`, "Unblacklists a user.");
 
         if (index.currentlyCooldowned[message.author.id] === message.author.id) return message.reply(embed.setDescription("Please wait a few seconds before executing the command again!")).then(msg => msg.delete( {timeout: 3000} ).then(message.delete( {timeout: 2000} )))
         functions.setCooldown(5, message.author.id, message);
